7de8ffa5fbac8382f2053d435c606d764cc3f60b
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-12-19 23:40:47 +0500

    without firebase

--- app.js
+++ app.js
@@ -7,31 +7,44 @@ let addTodo = () => {
   todoList.innerHTML =
     `
     <li class="li-item">
-    ${todoInput.value}
-    <button class="edit_btn">Edit</button>
+    <input class="li-input" type="text" value="${todoInput.value}" disabled >
+    <button id="edit_btn">Edit</button>
     <button onclick="deleteTodo()">delete</button>
   </li>
         ` + todoList.innerHTML;
   todoInput.value = "";
   addEventListener();
+  
 };
 todoBtn && todoBtn.addEventListener("click", addTodo);
-
+let edit = false
 let addEventListener = () => {
-  let edit_btn = document.querySelectorAll(".edit_btn");
-  edit_btn.forEach((button, i) => {
-    button.addEventListener("click", () => {
-      console.log();
-      editBtn(i);
-    });
+  let edit_btn = document.getElementById("edit_btn");
+  edit_btn.addEventListener("click", () => {
+    console.log(event.target.parentNode.childNodes[1].value)
+    if(edit){
+      event.target.parentNode.childNodes[1].disabled = true
+      event.target.parentNode.childNodes[1].blur()
+      event.target.childNodes[0].nodeValue = "Edit"
+      // todoInput.value = event.target.parentNode.childNodes[1].value
+      
+      console.log(event.target.parentNode.childNodes[1].value , todoInput.value);
+      edit = false
+    }else{
+      event.target.parentNode.childNodes[1].disabled = false
+      event.target.parentNode.childNodes[1].focus()
+      event.target.childNodes[0].nodeValue = "Update"
+      console.log(event.target.parentNode.childNodes[1].value);
+      edit = true
+    }
   });
 };
 
-const editBtn = (index) => {
-  let userPromt = prompt("enter a update value", "hello");
-  let li_item = document.querySelectorAll(".li-item");
-  event.target.parentNode.firstChild.nodeValue = userPromt;
-};
+// const editBtn = (index) => {
+//   let userPromt = prompt("enter a update value", "hello");
+//   let li_item = document.querySelectorAll(".li-item");
+//   event.target.parentNode.firstChild.nodeValue = userPromt;
+// };
 
 let deleteAll = () => {
   todoList.innerHTML = "";
