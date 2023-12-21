e3fdfb6b24ba6f161d245fd933ae90159226918e
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-12-21 18:58:30 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,93 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { collection, addDoc, getFirestore, onSnapshot , doc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbiomcJMnlH9b0zyHQFiUCJl08J_i6u6g",
+  authDomain: "todo-firebase-9f350.firebaseapp.com",
+  projectId: "todo-firebase-9f350",
+  storageBucket: "todo-firebase-9f350.appspot.com",
+  messagingSenderId: "595012601643",
+  appId: "1:595012601643:web:b368293310ff439b6a676e"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+
+
 let todoBtn = document.getElementById("todo-btn");
 let todoList = document.getElementById("todo-list");
 let todoInput = document.getElementById("todoInput");
 let deleteBtn = document.getElementById("delete-all");
+let time = moment().format(`l LT`)
+let edit = false
+let edit_btn = document.getElementById("edit_btn");
+// console.log(time)
 
-let addTodo = () => {
-  todoList.innerHTML =
-    `
-    <li class="li-item">
-    <input class="li-input" type="text" value="${todoInput.value}" disabled >
-    <button id="edit_btn">Edit</button>
-    <button onclick="deleteTodo()">delete</button>
-  </li>
-        ` + todoList.innerHTML;
-  todoInput.value = "";
-  addEventListener();
+
+let getTodo = () => {
+  onSnapshot(collection(db, "todos"), (data) => {
+    data.docChanges().forEach((todo) => {
+      // console.log("data======>", todo.doc.id)
+      todoList.innerHTML =
+        `
+        <li class="li-item">
+        <input class="li-input" type="text" value="${todo.doc.data().value}" disabled >
+        <p>${todo.doc.data().time}</p>
+        <button id="edit_btn">Edit</button>
+        <button onclick="deleteTodo('${todo.doc.id}')">delete</button>
+      </li>
+            ` + todoList.innerHTML;
+
+    })
+  });
+
+
+}
+getTodo()
+
+let addTodo = async () => {
+  try {
+
+    const docRef = await addDoc(collection(db, "todos"), {
+      value: todoInput.value,
+      time: time,
+
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (err) {
+    console.log(err)
+  }
   
+  todoInput.value = "";
+
+  // todoList.innerHTML =
+  //   `
+  //   <li class="li-item">
+  //   <input class="li-input" type="text" value="${todoInput.value}" disabled >
+  //   <button id="edit_btn">Edit</button>
+  //   <button onclick="deleteTodo()">delete</button>
+  // </li>
+  //       ` + todoList.innerHTML;
+  // addEventListener();
+
 };
 todoBtn && todoBtn.addEventListener("click", addTodo);
-let edit = false
 let addEventListener = () => {
-  let edit_btn = document.getElementById("edit_btn");
   edit_btn.addEventListener("click", () => {
     console.log(event.target.parentNode.childNodes[1].value)
-    if(edit){
+    if (edit) {
       event.target.parentNode.childNodes[1].disabled = true
       event.target.parentNode.childNodes[1].blur()
       event.target.childNodes[0].nodeValue = "Edit"
       // todoInput.value = event.target.parentNode.childNodes[1].value
-      
-      console.log(event.target.parentNode.childNodes[1].value , todoInput.value);
+
+      console.log(event.target.parentNode.childNodes[1].value, todoInput.value);
       edit = false
-    }else{
+    } else {
       event.target.parentNode.childNodes[1].disabled = false
       event.target.parentNode.childNodes[1].focus()
       event.target.childNodes[0].nodeValue = "Update"
@@ -52,7 +109,8 @@ let deleteAll = () => {
 
 deleteBtn && deleteBtn.addEventListener("click", deleteAll);
 
-let deleteTodo = () => {
-  event.target.parentNode.remove();
+let deleteTodo = async () => {
+  await deleteDoc(doc(db, "todos", id));
+  // event.target.parentNode.remove();
 };
 window.deleteTodo = deleteTodo;
